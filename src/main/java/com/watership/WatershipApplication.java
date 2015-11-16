d5a026b48b7fb84c10f37f96a7fddccb15a70f04
magnusfiorepalm <magnus.palm@procore.com>
2015-11-15 22:35:29 -0600

    Setting up project

---
+++ src/main/java/com/watership/WatershipApplication.java
@@ -0,0 +1,12 @@
+package com.watership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatershipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatershipApplication.class, args);
+    }
+}
