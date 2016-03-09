f083d706d3e874ee28db701ede0c708b89a4681a
magnusfiorepalm <magnus.palm@procore.com>
2016-03-08 17:16:23 -0800

    Adding support for CORS

--- src/main/java/com/watership/WatershipApplication.java
+++ src/main/java/com/watership/WatershipApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WatershipApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WatershipApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(WatershipApplication.class, args);
+	}
 }
